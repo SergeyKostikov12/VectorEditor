1e0f573036e2bf3caefcb5d0436b6096da1f4a28
Сергей Костиков <mastercnc@list.ru>
2021-09-17 02:29:59 +0700

    11

--- Functionality/Figure.cs
+++ Functionality/Figure.cs
@@ -4,9 +4,23 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
+
+interface IFigure
+{
+    void ShowOutline();
+    void HideOutline();
+    void MoveMarker(Point position);
+    void ExecuteRelize(Point position);
+    bool SelectMarker(Point poin);
+    bool SelectLine(Point point);
+    void DeselectFigure();
+    List<Rectangle> GetMarkers();
+    Polyline GetShape();
+}
+
 namespace GraphicEditor.Functionality
 {
-    public abstract class Figure
+    public abstract class Figure : IFigure
     {
         public FigureType FigureType { get; protected set; } 
         public Point AnchorPoint { get; set; }
@@ -15,26 +29,26 @@ namespace GraphicEditor.Functionality
         public SolidColorBrush Fill { get => GetFill(); set => SetFill(value); }
 
 
-        public abstract void ShowOutline();
-        public abstract void HideOutline();
-        public abstract void MoveMarker(Point position);
-        public abstract void ExecuteRelize(Point position);
-        public abstract bool SelectMarker(Point poin);
-        public abstract bool SelectLine(Point point);
-        public abstract void DeselectFigure();
-        public abstract List<Rectangle> GetMarkers();
-        public abstract Polyline GetShape();
-        public virtual Rectangle ExecuteDoubleClick(Point position) 
+        public abstract void ShowOutline(); //all
+        public abstract void HideOutline(); //all
+        public abstract void MoveMarker(Point position); //all
+        public abstract void ExecuteRelize(Point position); //all
+        public abstract bool SelectMarker(Point poin); //all
+        public abstract bool SelectLine(Point point); //all
+        public abstract void DeselectFigure(); //all
+        public abstract List<Rectangle> GetMarkers(); //all
+        public abstract Polyline GetShape(); //all
+        public virtual Rectangle ExecuteDoubleClick(Point position) //line
         {
             return null; 
         }
 
-        protected abstract int GetStrokeWidth();
-        protected abstract void SetStrokeWidth(int value);
-        protected abstract SolidColorBrush GetFill();
-        protected abstract void SetFill(SolidColorBrush brush);
-        protected abstract SolidColorBrush GetLineColor();
-        protected abstract void SetLineColor(SolidColorBrush colorBrush);
+        protected abstract int GetStrokeWidth(); //all
+        protected abstract void SetStrokeWidth(int value); //all
+        protected abstract SolidColorBrush GetFill(); //all
+        protected abstract void SetFill(SolidColorBrush brush); //all
+        protected abstract SolidColorBrush GetLineColor(); //all
+        protected abstract void SetLineColor(SolidColorBrush colorBrush); //all
 
 
 
